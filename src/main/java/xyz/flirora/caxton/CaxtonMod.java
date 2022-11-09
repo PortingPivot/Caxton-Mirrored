34a2a711e32a8a4b4200ee815cf475a0a8f62fe1
+merlan #flirora <uruwi@protonmail.com>
2022-11-09 09:12:23 -0500

    Use bindings to HarfBuzz
    :concern:

--- src/main/java/xyz/flirora/caxton/CaxtonMod.java
+++ src/main/java/xyz/flirora/caxton/CaxtonMod.java
@@ -1,10 +1,33 @@
 package xyz.flirora.caxton;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.MinecraftClient;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 public class CaxtonMod implements ModInitializer {
     @Override
     public void onInitialize() {
         System.out.println("initialize");
+
+        File runDirectory = MinecraftClient.getInstance().runDirectory;
+
+        try (var libStream = getClass().getResourceAsStream("/linux-x86-64/libcaxtonbindings.so")) {
+            if (libStream == null) {
+                throw new FileNotFoundException("Could not find /linux-x86-64/libcaxtonbindings.so");
+            }
+
+            File tmp = new File(runDirectory, "libcaxtonbindings.so");
+            try (var output = new FileOutputStream(tmp)) {
+                libStream.transferTo(output);
+            }
+
+            System.load(tmp.toString());
+            System.loadLibrary("harfbuzz");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/xyz/flirora/caxton/font/CaxtonFont.java
+++ src/main/java/xyz/flirora/caxton/font/CaxtonFont.java
@@ -1,5 +1,7 @@
 package xyz.flirora.caxton.font;
 
+import xyz.flirora.bindings.*;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.MessageDigest;
@@ -7,28 +9,37 @@ import java.util.Base64;
 
 /**
  * Holds information related to a single font file.
- * <p>
- * At the moment, this uses functionality from AWT. This unfortunately means
- * that this mod might not work properly in macOS, in which case go R.L. yourself.
- * <p>
- * Still, no references to AWT classes should be made outside this class, in case
- * we discover a way to shape text in Java without AWT.
  */
-public class CaxtonFont {
+public class CaxtonFont implements AutoCloseable {
     private static final byte[] SALT = {-0x1A, 0x26, 0x69, 0x11};
 
-    private final java.awt.Font font;
+    private final SWIGTYPE_p_hb_font_t font;
 
     public CaxtonFont(InputStream input) throws IOException {
+        SWIGTYPE_p_hb_blob_t blob = null;
+        SWIGTYPE_p_hb_face_t face = null;
+
         try {
+            byte[] readInput = input.readAllBytes();
+
             MessageDigest digest = MessageDigest.getInstance("SHA-1");
-            digest.update(input.readAllBytes());
+            digest.update(readInput);
             byte[] hash = digest.digest(SALT);
             String cacheFolder = Base64.getEncoder().encodeToString(hash);
-            input.reset();
-            this.font = java.awt.Font.createFonts(input)[0];
+
+            blob = Harfbuzz.hb_blob_create(readInput, hb_memory_mode_t.HB_MEMORY_MODE_DUPLICATE, null, null);
+            face = Harfbuzz.hb_face_create(blob, 0);
+            this.font = Harfbuzz.hb_font_create(face);
         } catch (Exception e) {
             throw new RuntimeException(e);
+        } finally {
+            Harfbuzz.hb_blob_destroy(blob);
+            Harfbuzz.hb_face_destroy(face);
         }
+    }
+
+    @Override
+    public void close() throws Exception {
+        Harfbuzz.hb_font_destroy(this.font);
     }
 }
