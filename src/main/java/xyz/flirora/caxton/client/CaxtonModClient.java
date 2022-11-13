d5817c1289a1d5687ef9cce8cb0d4b8e8bddea5f
+merlan #flirora <uruwi@protonmail.com>
2022-11-13 16:59:41 -0500

    Add built-in resource pack for changing the default font

--- src/main/java/xyz/flirora/caxton/client/CaxtonModClient.java
+++ src/main/java/xyz/flirora/caxton/client/CaxtonModClient.java
@@ -3,6 +3,10 @@ package xyz.flirora.caxton.client;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
@@ -12,6 +16,8 @@ import xyz.flirora.caxton.mixin.TextRendererAccessor;
 
 @Environment(EnvType.CLIENT)
 public class CaxtonModClient implements ClientModInitializer {
+    public static final String MOD_ID = "caxton";
+
     public static void testRunLister(MinecraftClient client) {
         System.out.println("testing run lister...");
 
@@ -32,5 +38,9 @@ public class CaxtonModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         System.out.println("initialize client");
+
+        ModContainer modContainer = FabricLoader.getInstance().getModContainer(MOD_ID).get();
+
+        ResourceManagerHelper.registerBuiltinResourcePack(new Identifier(MOD_ID, "inter"), modContainer, ResourcePackActivationType.NORMAL);
     }
 }
